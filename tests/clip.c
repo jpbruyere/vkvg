c10e6d308d39ddbe4c98c4411bbf7c8f7ffddf2e
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2019-04-28 00:45:59 +0200

    .deb generation, subpass access flag debug

--- tests/clip.c
+++ tests/clip.c
@@ -1,6 +1,7 @@
 #include "test.h"
 
 void test_clip(){
+    vkvg_surface_clear(surf);
     VkvgContext ctx = vkvg_create(surf);
 
     vkvg_move_to(ctx,10,10);
@@ -11,14 +12,35 @@ void test_clip(){
     vkvg_line_to(ctx,500,650);
     vkvg_line_to(ctx,100,800);
     vkvg_line_to(ctx,150,400);
+    vkvg_clip_preserve(ctx);
+    vkvg_set_operator(ctx, VKVG_OPERATOR_CLEAR);
+    vkvg_fill_preserve(ctx);
     vkvg_clip(ctx);
+    vkvg_set_operator(ctx, VKVG_OPERATOR_OVER);
 
     vkvg_set_source_rgb(ctx,1,0,0);
     vkvg_paint(ctx);
 
     vkvg_destroy(ctx);
 }
+void test_clip2(){
+    vkvg_surface_clear(surf);
+    VkvgContext ctx = vkvg_create(surf);
 
+    vkvg_rectangle(ctx, 50,50,200,200);
+    //vkvg_clip(ctx);
+    vkvg_rectangle(ctx, 50,50,200,200);
+    /*vkvg_clip_preserve(ctx);
+    vkvg_set_operator(ctx, VKVG_OPERATOR_CLEAR);
+    vkvg_fill_preserve(ctx);*/
+    vkvg_clip(ctx);
+    //vkvg_set_operator(ctx, VKVG_OPERATOR_OVER);
+
+    vkvg_set_source_rgb(ctx,1,0,0);
+    vkvg_paint(ctx);
+
+    vkvg_destroy(ctx);
+}
 int main(int argc, char *argv[]) {
 
     perform_test (test_clip, 1024, 768);
