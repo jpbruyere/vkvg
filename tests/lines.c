9cc9fbfeb2ff47f2d62ac35dd061b8fbbfda1a4d
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2019-09-02 05:18:18 +0200

    add perf tests

--- tests/lines.c
+++ tests/lines.c
@@ -7,24 +7,24 @@ void test(){
 
     srand((unsigned) currentTime.tv_usec);
     const float w = 1024.f;
+    const float h = 800.f;
 
     VkvgContext ctx = vkvg_create(surf);
-    vkvg_set_fill_rule(ctx, VKVG_FILL_RULE_EVEN_ODD);
+    //vkvg_set_fill_rule(ctx, VKVG_FILL_RULE_EVEN_ODD);
     vkvg_set_line_width(ctx,2);
     //vkvg_set_line_join(ctx,VKVG_LINE_JOIN_BEVEL);
     vkvg_set_source_rgba(ctx,1.0,0.0,0.0,0.1);
+
     for (int i=0; i<250; i++) {
         randomize_color(ctx);
-        float x = trunc( (0.5*(float)w*rand())/RAND_MAX );
-        float y = trunc( (0.5*(float)w*rand())/RAND_MAX );
-        float z = trunc( (0.5*(float)w*rand())/RAND_MAX ) + 1;
-        float v = trunc( (0.5*(float)w*rand())/RAND_MAX ) + 1;
+        float x1 = trunc( ((float)w*rand())/RAND_MAX );
+        float y1 = trunc( ((float)h*rand())/RAND_MAX );
+        float x2 = trunc( ((float)w*rand())/RAND_MAX ) + 1;
+        float y2 = trunc( ((float)h*rand())/RAND_MAX ) + 1;
 
-        vkvg_rectangle(ctx, x+1, y+1, z, v);
-        /*vkvg_fill_preserve(ctx);
-        randomize_color(ctx);*/
-        vkvg_fill(ctx);
-        vkvg_stroke(ctx);
+        vkvg_move_to (ctx, x1, y1);
+        vkvg_line_to (ctx, x2, y2);
+        vkvg_stroke (ctx);
     }
     vkvg_destroy(ctx);
 }
--- tests/rect_fill.c
+++ tests/rect_fill.c
@@ -1,15 +1,26 @@
 #include "test.h"
 
 void test(){
+    vkvg_matrix_t mat;
+    vkvg_matrix_init_identity(&mat);
+    vkvg_matrix_scale(&mat,zoom,zoom);
+    vkvg_matrix_translate(&mat, panX, panY);
+
     vkvg_surface_clear(surf);
+
     VkvgContext ctx = vkvg_create(surf);
+    vkvg_set_source_rgba(ctx,1.0f,1.0f,1.0f,1);
+    vkvg_paint(ctx);
+
+    vkvg_set_matrix(ctx,&mat);
+
     vkvg_set_fill_rule(ctx, VKVG_FILL_RULE_EVEN_ODD);
     vkvg_set_line_width(ctx,10);
-    vkvg_set_source_rgba(ctx,0,0,1,0.5);
+    vkvg_set_source_rgba(ctx,0,0,1,1);
     vkvg_rectangle(ctx,100,100,200,200);
-    vkvg_fill(ctx);
+    /*vkvg_fill(ctx);
     vkvg_rectangle(ctx,200,200,200,200);
-    vkvg_set_source_rgba(ctx,1,0,0,0.5);
+    vkvg_set_source_rgba(ctx,1,0,0,0.5);*/
     vkvg_fill(ctx);
 
     vkvg_destroy(ctx);
