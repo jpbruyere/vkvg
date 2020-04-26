bf301c7f74749a84bf33ce2da4ce82726b0dc817
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2020-04-26 10:55:12 +0200

    tests

--- tests/circles.c
+++ tests/circles.c
@@ -3,15 +3,19 @@
 void test(){
     VkvgContext ctx = vkvg_create(surf);
 
+
     vkvg_set_line_width(ctx, 1);
     vkvg_set_source_rgb   (ctx, 1,1,1);
     vkvg_paint(ctx);
     vkvg_set_source_rgb   (ctx, 0,0,0);
 
+    vkvg_scale(ctx,10.0f,10.0f);
+    //vkvg_scale(ctx,2.0f,2.0f);
+
     float x = 10;
     float r = 1;
     while (x < 600){
-        vkvg_arc(ctx, x, 300,   r, 0, M_PI*2);
+        vkvg_arc(ctx, x+0.5f, 10.5f,   r, 0, M_PI*2);
         x += r;
         r ++;
         x += r + 5;
--- tests/dashes.c
+++ tests/dashes.c
@@ -6,19 +6,34 @@ void test(){
 
     VkvgContext ctx = vkvg_create(surf);
     //const float dashes[] = {160.0f, 80};
-    //const float dashes[] = {160.0f, 30};
-    const float dashes[] = {50, 40};
+    float dashes[] = {700.0f, 30};
+    //const float dashes[] = {50, 40};
     vkvg_set_line_cap(ctx, VKVG_LINE_CAP_ROUND);
     vkvg_set_dash(ctx, dashes, 2, offset);
     vkvg_set_line_width(ctx, 20);
     vkvg_set_source_rgb(ctx, 0, 0, 1);
 
-    vkvg_move_to (ctx, 50, 50);
+    vkvg_move_to (ctx, 150, 50);
     vkvg_rel_line_to (ctx, 500, 0);
-    vkvg_rel_line_to (ctx, 0, 500);
-    vkvg_rel_line_to (ctx, 200, 0);
     vkvg_rel_line_to (ctx, 0, 200);
+    vkvg_rel_line_to (ctx, 200, 0);
+    vkvg_rel_line_to (ctx, 0, 500);
     vkvg_rel_line_to (ctx, -700, 0);
+    vkvg_close_path(ctx);
+    vkvg_stroke (ctx);
+
+    dashes[0] = 0;
+    dashes[1] = 30;
+    vkvg_set_dash(ctx, dashes, 2, offset);
+
+    vkvg_set_source_rgb(ctx, 0, 1, 0);
+
+    vkvg_move_to (ctx, 200, 100);
+    vkvg_rel_line_to (ctx, 400, 0);
+    vkvg_rel_line_to (ctx, 0, 200);
+    vkvg_rel_line_to (ctx, 200, 0);
+    vkvg_rel_line_to (ctx, 0, 400);
+    vkvg_rel_line_to (ctx, -600, 0);
     vkvg_close_path(ctx);
     vkvg_stroke (ctx);
 
