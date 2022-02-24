b00d9c919f2d6889a29c12e00ae4da279fa613d3
Marco Rubin <20150305+Rubo3@users.noreply.github.com>
2022-02-24 14:34:30 +0000

    Update vkvg_has_current_point

--- src/vkvg_context.c
+++ src/vkvg_context.c
@@ -526,7 +526,7 @@ void vkvg_move_to (VkvgContext ctx, float x, float y)
 bool vkvg_has_current_point (VkvgContext ctx) {
     if (ctx->status)
         return false;
-    return true;
+    return _current_path_is_empty(ctx);
 
 void vkvg_get_current_point (VkvgContext ctx, float* x, float* y) {
 	if (_current_path_is_empty(ctx)) {
