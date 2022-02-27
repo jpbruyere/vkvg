f85607edc5ecd61cadcb0670b3bbd9c41cca5a19
Marco Rubin <20150305+Rubo3@users.noreply.github.com>
2022-02-27 12:01:36 +0000

    Add vkvg_has_current_point and vkvg_get_target (#97)
    * Define vkvg_has_current_point and vkvg_get_target
    
    * Declare vkvg_has_current_point and vkvg_get_target

--- src/vkvg_context.c
+++ src/vkvg_context.c
@@ -520,6 +520,11 @@ void vkvg_move_to (VkvgContext ctx, float x, float y)
 	_finish_path(ctx);
 	_add_point (ctx, x, y);
 }
+bool vkvg_has_current_point (VkvgContext ctx) {
+	if (ctx->status)
+		return false;
+	return !_current_path_is_empty(ctx);
+}
 void vkvg_get_current_point (VkvgContext ctx, float* x, float* y) {
 	if (_current_path_is_empty(ctx)) {
 		*x = *y = 0;
@@ -1580,4 +1585,10 @@ void vkvg_ellipse (VkvgContext ctx, float radiusX, float radiusY, float x, float
 	vkvg_curve_to (ctx, bottomRightX, bottomRightY, topRightX, topRightY, topCenterX, topCenterY);
 	vkvg_curve_to (ctx, topLeftX, topLeftY, bottomLeftX, bottomLeftY, bottomCenterX, bottomCenterY);
 	vkvg_close_path (ctx);
+}
+
+VkvgSurface vkvg_get_target (VkvgContext ctx) {
+	if (ctx->status)
+		return NULL;
+	return ctx->pSurf;
 }
