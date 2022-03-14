f9947f918c68e714fbae006c9f803084173d4205
Marco Rubin <20150305+Rubo3@users.noreply.github.com>
2022-03-14 22:12:11 +0000

    Nevermind...

--- src/vkvg_context.c
+++ src/vkvg_context.c
@@ -1186,9 +1186,9 @@ void vkvg_show_text_with_length (VkvgContext ctx, const char* text, const int le
 		return;
 	RECORD(ctx, VKVG_CMD_SHOW_TEXT_WITH_LENGTH, text, length);
 	LOG(VKVG_LOG_INFO_CMD, "CMD: show_text_with_length:\n");
-	// setlocale(LC_CTYPE,"");
+	setlocale(LC_CTYPE, "");
 	const int glyph_count = mbsnrtowcs(NULL, &text, length, 0, NULL);
-	// printf("Error: %s\n", strerror(errno));
+	printf("Error: %s\n", strerror(errno));
 	_font_cache_show_text (ctx, text, glyph_count);
 }
 
