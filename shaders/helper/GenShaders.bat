88a0874f4d8f3cc08d0b68465f8165fbf811f8c6
LinuxLegend <Tyler@linuxdev.app>
2022-03-26 16:17:01 -0600

    Batch script correction

--- shaders/helper/GenShaders.bat
+++ shaders/helper/GenShaders.bat
@@ -1,14 +1,14 @@
-$GLSLC_BIN $GLSLDEFS $1 -o "shader_comp.spv"
+$GLSLC_BIN $GLSLDEFS %1 -o "shader_comp.spv"
 
-$GLSLC_BIN $GLSLDEFS $2 -o "shader2_comp.spv"
+$GLSLC_BIN $GLSLDEFS %2 -o "shader2_comp.spv"
 
-$GLSLC_BIN $GLSLDEFS $3 -o "vkvg_main_lcd_frag.spv"
+$GLSLC_BIN $GLSLDEFS %3 -o "vkvg_main_lcd_frag.spv"
 
-$GLSLC_BIN $GLSLDEFS $4 -o "vkvg_main_frag.spv"
+$GLSLC_BIN $GLSLDEFS %4 -o "vkvg_main_frag.spv"
 
-$GLSLC_BIN $GLSLDEFS $5 -o "vkvg_main_vert.spv"
+$GLSLC_BIN $GLSLDEFS %5 -o "vkvg_main_vert.spv"
 
-$GLSLC_BIN $GLSLDEFS $6 -o "wired_frag.spv"
+$GLSLC_BIN $GLSLDEFS %6 -o "wired_frag.spv"
 
 $XXD_BIN -i "shader_comp.spv" > "shaders.h"
 $XXD_BIN -i "shader2_comp.spv" >> "shaders.h"
