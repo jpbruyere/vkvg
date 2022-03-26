5ab35de4709db723253221e398f97c673f928eb8
LinuxLegend <Tyler@linuxdev.app>
2022-03-26 15:49:53 -0600

    Usable implementation of shader compilation

---
+++ shaders/helper/GenShaders.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Just checking"
+echo $1
+echo $2
+echo "Done checking"
+$GLSLC_BIN $GLSLDEFS $1 -o "shader_comp.spv"
+
+$GLSLC_BIN $GLSLDEFS $2 -o "shader2_comp.spv"
+
+$GLSLC_BIN $GLSLDEFS $3 -o "vkvg_main_lcd_frag.spv"
+
+$GLSLC_BIN $GLSLDEFS $4 -o "vkvg_main_frag.spv"
+
+$GLSLC_BIN $GLSLDEFS $5 -o "vkvg_main_vert.spv"
+
+$GLSLC_BIN $GLSLDEFS $6 -o "wired_frag.spv"
+
+$XXD_BIN -i "shader_comp.spv" > "shaders.h"
+$XXD_BIN -i "shader2_comp.spv" >> "shaders.h"
+$XXD_BIN -i "vkvg_main_lcd_frag.spv" >> "shaders.h"
+$XXD_BIN -i "vkvg_main_frag.spv" >> "shaders.h"
+$XXD_BIN -i "vkvg_main_vert.spv" >> "shaders.h"
+$XXD_BIN -i "wired_frag.spv" >> "shaders.h"
