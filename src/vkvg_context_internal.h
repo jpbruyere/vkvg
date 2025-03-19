1f23dd482f4236f9a9758a9bf2ecd9642c0a2c93
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2025-03-19 18:19:31 +0100

    rgb color conversion with byte mask

--- src/vkvg_context_internal.h
+++ src/vkvg_context_internal.h
@@ -56,10 +56,10 @@
 #define FULLSCREEN_BIT         0x10000000
 #define SRCTYPE_MASK           0x000000FF
 
-#define CreateRgba(r, g, b, a) ((a << 24) | (r << 16) | (g << 8) | b)
+#define CreateRgba(r, g, b, a) (((a&0xFF) << 24) | ((r&0xFF) << 16) | ((g&0xFF) << 8) | b)
 #ifdef VKVG_PREMULT_ALPHA
 #define CreateRgbaf(r, g, b, a)                                                                                        \
-    (((int)(a * 255.0f) << 24) | ((int)(b * a * 255.0f) << 16) | ((int)(g * a * 255.0f) << 8) | (int)(r * a * 255.0f))
+    ((((uint32_t)(a * 255.0f)&0xFF) << 24) | (((uint32_t)(b * a * 255.0f)&0xFF) << 16) | (((uint32_t)(g * a * 255.0f)&0xFF) << 8) | ((uint32_t)(r * a * 255.0f)&0xFF))
 #else
 #define CreateRgbaf(r, g, b, a)                                                                                        \
     (((int)(a * 255.0f) << 24) | ((int)(b * 255.0f) << 16) | ((int)(g * 255.0f) << 8) | (int)(r * 255.0f))
